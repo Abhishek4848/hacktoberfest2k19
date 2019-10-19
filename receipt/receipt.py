c9249710bb222d52190ea9107b860761e87bdfba
Abhishek4848 <54174670+Abhishek4848@users.noreply.github.com>
2019-10-20 04:27:00 +0530

    debugged the reciept generator pogram

--- receipt/receipt.py
+++ receipt/receipt.py
@@ -5,15 +5,18 @@ stylish_settee_description = """Stylish Settee. Faux leather on birch. 29.50 inc
 #price
 stylish_settee_price = 180.50
 luxurious_lamp_description = """Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."""
+#price
 luxurious_lamp_price = 52.15
 sales_tax = .088
 customer_one_total = 0
 customer_one_itemization = ""
-customer_one_total += customer_one_total + lovely_loveseat_price
-customer_one_itemization += customer_one_itemization + lovely_loveseat_description
-customer_one_total += customer_one_total + luxurious_lamp_price
-customer_one_itemization += customer_one_itemization + luxurious_lamp_description
+customer_one_total += lovely_loveseat_price
+customer_one_itemization +=lovely_loveseat_description+"\n"
+customer_one_total += stylish_settee_price
+customer_one_itemization +=stylish_settee_description+"\n"
+customer_one_total +=luxurious_lamp_price
+customer_one_itemization +=luxurious_lamp_description+"\n"
 customer_one_tax = customer_one_total * sales_tax
-customer_one_total += customer_one_total + customer_one_tax
-print("Customer One Items: " , customer_one_itemization)
+customer_one_total +=customer_one_tax
+print("Customer One Items: \n " , customer_one_itemization)
 print("Customer One Total: " , (customer_one_total))
